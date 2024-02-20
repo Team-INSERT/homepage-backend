981b64df3508322142032423c8169c5e6a5e0e32
anys34 <anyeseong34@gmail.com>
2024-02-20 15:55:48 +0900

    init

---
+++ src/main/kotlin/com/teaminsert/homepage/HomepageApplication.kt
@@ -0,0 +1,11 @@
+package com.teaminsert.homepage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomepageApplication
+
+fun main(args: Array<String>) {
+	runApplication<HomepageApplication>(*args)
+}
