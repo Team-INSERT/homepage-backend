fece9f2322c697cda3bb838bd592be36aaf8b0db
anys34 <anyeseong34@gmail.com>
2024-02-22 13:21:39 +0900

    Feat :: Security

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
---
+++ src/main/kotlin/com/teaminsert/homepage/global/config/SecurityConfig.kt
@@ -0,0 +1,34 @@
+package com.teaminsert.homepage.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.csrf().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .logout().disable()
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        http.cors().and()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+
+        http.exceptionHandling().authenticationEntryPoint(
+                HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+
+        return http.build()
+    }
+}
