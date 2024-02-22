caca1857e4b6edb03c46d0782363b4c059138d02
anys34 <anyeseong34@gmail.com>
2024-02-22 18:39:12 +0900

    Update :: picture

--- src/main/kotlin/com/teaminsert/homepage/global/feign/auth/dto/res/GoogleInformationResponse.kt
+++ src/main/kotlin/com/teaminsert/homepage/global/feign/auth/dto/res/GoogleInformationResponse.kt
@@ -3,6 +3,5 @@ package com.teaminsert.homepage.global.feign.auth.dto.res
 class GoogleInformationResponse(
         val email: String,
         val name: String,
-        val picture: String
 ) {
 }
