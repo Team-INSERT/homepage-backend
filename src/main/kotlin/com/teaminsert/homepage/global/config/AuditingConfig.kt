57961b86f9d6ec05f3934875b2b1b4ee113dec0d
anys34 <anyeseong34@gmail.com>
2024-02-23 13:35:05 +0900

    Feat :: BaseTimeEntity

---
+++ src/main/kotlin/com/teaminsert/homepage/global/config/AuditingConfig.kt
@@ -0,0 +1,9 @@
+package com.teaminsert.homepage.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class AuditingConfig {
+}
---
+++ src/main/kotlin/com/teaminsert/homepage/global/entity/BaseTimeEntity.kt
@@ -0,0 +1,18 @@
+package com.teaminsert.homepage.global.entity
+
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+class BaseTimeEntity {
+    @CreatedDate
+    var createdAt: LocalDateTime? = null
+
+    @LastModifiedBy
+    var updatedAt: LocalDateTime? = null
+}
