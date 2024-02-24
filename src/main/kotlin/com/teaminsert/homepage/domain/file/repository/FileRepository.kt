408cbd3f8dd6b738c8a4192bdf3c9d1ff03e0ac8
anys34 <anyeseong34@gmail.com>
2024-02-24 23:30:49 +0900

    Delete :: file Entity

--- src/main/kotlin/com/teaminsert/homepage/domain/file/repository/FileRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.teaminsert.homepage.domain.file.repository
-
-import com.teaminsert.homepage.domain.file.domain.File
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface FileRepository: JpaRepository<File, Long> {
-}
