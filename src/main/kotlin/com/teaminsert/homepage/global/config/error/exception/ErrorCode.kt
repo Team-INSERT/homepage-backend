2354ff2324e01cc62c60c9e40e4517faffa8a279
anys34 <anyeseong34@gmail.com>
2024-03-02 21:31:01 +0900

    만료된 토큰 리턴 상태코드 변경

--- src/main/kotlin/com/teaminsert/homepage/global/config/error/exception/ErrorCode.kt
+++ src/main/kotlin/com/teaminsert/homepage/global/config/error/exception/ErrorCode.kt
@@ -8,7 +8,7 @@ enum class ErrorCode(
 ) {
     USER_NOT_MATCH(HttpStatus.BAD_REQUEST, "User Not Match"),
     SCHOOL_USER_NOT(HttpStatus.BAD_REQUEST, "School User Not"),
+    EXPIRED_JWT(HttpStatus.BAD_REQUEST, "Expired Jwt"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User Not Found"),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post Not Found"),
-    EXPIRED_JWT(HttpStatus.UNAUTHORIZED, "Expired Jwt"),
 }
