f7688905c0d4ff588b0f62aac31ef8df930f19d9
anys34 <anyeseong34@gmail.com>
2024-03-16 21:07:29 +0900

    post API

--- src/main/kotlin/com/teaminsert/homepage/domain/post/presentation/PostController.kt
+++ src/main/kotlin/com/teaminsert/homepage/domain/post/presentation/PostController.kt
@@ -2,10 +2,13 @@ package com.teaminsert.homepage.domain.post.presentation
 
 import com.teaminsert.homepage.domain.post.presentation.dto.req.PostRequest
 import com.teaminsert.homepage.domain.post.service.*
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
+@Tag(name = "post API")
 @RequestMapping("/post")
 @RestController
 class PostController(
@@ -16,26 +19,31 @@ class PostController(
         private val updateService: UpdateService,
         private val deleteService: DeleteService
 ) {
+    @Operation(summary = "게시글 리스트 조회")
     @GetMapping
     fun list(category: String, pageable: Pageable)
         = listService.execute(category, pageable)
 
+    @Operation(summary = "세부 게시글 조회")
     @GetMapping("/id")
     fun post(id: Long)
         = postService.execute(id)
 
+    @Operation(summary = "게시글 등록")
     @PostMapping
     fun save(@RequestBody postRequest: PostRequest)
         = saveService.execute(postRequest)
 
+    @Operation(summary = "파일 링크로 변환")
     @PostMapping("/file")
     fun file(@RequestPart file: MultipartFile)
         = uploadService.execute(file)
-
+    @Operation(summary = "게시글 수정")
     @PutMapping
     fun update(@RequestBody postRequest: PostRequest)
         = updateService.execute(postRequest)
 
+    @Operation(summary = "게시글 삭제")
     @DeleteMapping
     fun delete(id: Long)
         = deleteService.execute(id)
